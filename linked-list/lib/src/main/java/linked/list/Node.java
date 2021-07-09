fc09a5b24eccdbdaa9f3d9041773999bde4041f4
alaa281997 <alaabbas97@gmail.com>
2021-07-09 16:39:21 +0300

    add challenge5

---
+++ linked-list/lib/src/main/java/linked/list/Node.java
@@ -0,0 +1,13 @@
+package main.java.linked.list;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+
+}
