f2bc52684a8fc738267a86164753f8c58f247248
alaa281997 <alaabbas97@gmail.com>
2021-07-25 23:45:33 +0300

    Code challenge 16

--- trees/lib/src/main/java/trees/BinarySearchTree.java
+++ trees/lib/src/main/java/trees/BinarySearchTree.java
@@ -45,4 +45,10 @@ public class BinarySearchTree {
         return false;
     }
 
+    @Override
+    public String toString() {
+        return "BinarySearchTree{" +
+                "root=" + root.getKey() +
+                '}';
+    }
 }
